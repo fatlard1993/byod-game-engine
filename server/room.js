26941afe3808502c037a44076c25f656cc48c368
justfatlard <fatlard1993@gmail.com>
2021-07-05 18:21:12 -0700

    Fix removePlayer

--- server/room.js
+++ server/room.js
@@ -2,24 +2,24 @@ const { nanoid } = require('nanoid');
 const log = new (require('log'))({ tag: 'byod-game-engine' });
 
 class Room {
-	constructor(options) {
+	constructor({ ...options, name }) {
 		this.id = nanoid(7);
 		this.options = options;
-		this.name = options.name;
+		this.name = name;
 		this.players = {};
 		this.playerIds = [];
 		this.state = {};
 
-		log(`[room - ${this.name}] Create`);
+		log(`Created Game Room "${this.name}"`);
 		log(1)(options);
 	}
 
 	addPlayer(player){
-		log(`[room - ${this.name}] User "${player.name}" join`);
+		log(`[room - ${this.name}] Player "${player.name || player.id || 'anonymous'}" joining`);
 
 		player.state = 'joining';
 
-		const existingPlayer = this.getPlayerByName(player.name);
+		const existingPlayer = (player.id && this.players[player.id]) || (player.name && this.getPlayerByName(player.name));
 
 		if (existingPlayer) {
 			this.players[existingPlayer.id] = { ...existingPlayer, ...player };
@@ -42,17 +42,17 @@ class Room {
 	}
 
 	removePlayer(player){
-		if(!player || !player.name || !this.players[player.name]) return;
+		if(!player || !player.id || !this.players[player.id]) return;
 
-		log(`[room - ${this.name}] Player "${player.name}" leave`);
+		log(`[room - ${this.name}] Player "${player.name || player.id || 'anonymous'}" leaving`);
 
 		this.players[player.name].state = 'inactive';
 	}
 
 	broadcast(type, payload){
-		log(1)('broadcast', type, payload, this.playerIds);
+		log(1)(`[room - ${this.name}] broadcast`, type, payload, this.playerIds);
 
-		var message = JSON.stringify({ type, payload });
+		const message = JSON.stringify({ type, payload });
 
 		this.playerIds.forEach((id) => {
 			const socket = this.players[id].socket;
